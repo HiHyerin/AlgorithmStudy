642bb4d67b9af2ec21e6cb566735c884420d5d40
HiHyerin <116174672+HiHyerin@users.noreply.github.com>
2024-09-30 09:04:53 +0900

    [level 0] Title: A 강조하기, Time: 0.07 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder answer = new StringBuilder();
+
+        for(char c:myString.toCharArray()) {
+            if(c == 'a' || c == 'A') {
+                answer.append(Character.toUpperCase(c));
+            } else {
+                answer.append(Character.toLowerCase(c));
+            }
+        }
+        return answer.toString();
+    }
+}
