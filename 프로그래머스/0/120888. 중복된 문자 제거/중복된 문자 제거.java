7f7d0dbc47471239382326118157c738fc13be6d
HiHyerin <116174672+HiHyerin@users.noreply.github.com>
2025-04-04 16:13:48 +0900

    [level 0] Title: 중복된 문자 제거, Time: 14.22 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        Set<Character> set = new LinkedHashSet<>();
+        for(char c:my_string.toCharArray()) {
+            set.add(c);
+        }
+        for(char c : set) {
+            answer += c;
+        }
+        return answer;
+    }
+}
