7b0b69e09be07e045b9cc7b039a677d7af43388d
HiHyerin <116174672+HiHyerin@users.noreply.github.com>
2025-11-17 12:47:50 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.14 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        Map<String,Integer> map = new HashMap<>();
+        map.put("zero", 0);
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
+        map.put("six", 6);
+        map.put("seven", 7);
+        map.put("eight", 8);
+        map.put("nine", 9);
+        for(Map.Entry<String,Integer> entry : map.entrySet()) {
+            if(s.contains(entry.getKey())) {
+                s = s.replace(entry.getKey(), String.valueOf(entry.getValue()));
+            }
+        }
+        return Integer.parseInt(s);
+    }
+}
