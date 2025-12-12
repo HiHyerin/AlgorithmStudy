41bff001a8df50af83578b9de2f7e396e393be58
HiHyerin <116174672+HiHyerin@users.noreply.github.com>
2025-12-12 17:28:45 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,13 +2,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        for (int i = 0; i < commands.length; i++) {
-            // 1. array를 [][0]부터 [][1]까지 자르기
-            int[] arrayCopy = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            // 2. 오름차순 정렬
+        for(int i=0; i<commands.length; i++) {
+            int[] arrayCopy = new int[commands[i][1] - commands[i][0] + 1];
+            int k = 0;
+            for(int j=commands[i][0]-1; j< commands[i][1]; j++) {
+                arrayCopy[k] = array[j];
+                k++;
+            }
             Arrays.sort(arrayCopy);
-            // 3. [][2]번째 숫자 answer에 넣기
-            answer[i] = arrayCopy[commands[i][2]-1];
+            answer[i] = arrayCopy[commands[i][2] - 1];
         }
         return answer;
     }
